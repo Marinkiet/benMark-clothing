6b3b05fe96e59228fade6e2a7f274fbec1960c85
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-01-25 22:59:50 +0200

    menu items with router hoc to navigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './menu-item.styles.scss';
+import {withRouter } from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size})=>(
-<div className={`${size} menu-item`}>
+const MenuItem = ({title,imageUrl,size,linkUrl,history,match})=>(
+
+
+
+<div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image"
     style={{
         backgroundImage:`url(${imageUrl})`
-    }}/>
+    }}
+    />
     <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">
@@ -16,4 +21,6 @@ const MenuItem = ({title,imageUrl,size})=>(
 </div>
 );
 
-export default MenuItem;
+
+
+export default withRouter(MenuItem);
